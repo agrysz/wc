09404f4f6c4ceab657ced3ee5e2b560b0769deb1
agrysz <agrysz90@gmail.com>
2013-06-02 19:57:45 +0200

    On branch master Changes to be committed:   (use "git reset HEAD <file>..." to unstage) 	new file:   readme.txt 	modified:   src/webcrawler/WebCrawler.java
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   src/webcrawler/WebCrawler.java

--- src/webcrawler/WebCrawler.java
+++ src/webcrawler/WebCrawler.java
@@ -1,19 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package webcrawler;
 
-/**
- *
- * @author Adam
- */
 public class WebCrawler {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
